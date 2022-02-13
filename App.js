cabf3c0269b16ba6768a4e0d74495b652106bef1
tony dugué <duguetony@gmail.com>
2022-02-13 02:39:53 +0100

    add custom button component

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
+
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <ViewImageScreen />
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <AppButton title={"login"} onPress={ () => console.log('tapped')} />
+    </View>
+  )
 }
 
