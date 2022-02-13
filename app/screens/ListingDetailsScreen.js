d90d3a5c1f67fb103b8ce638a107f8023a909903
tony dugué <duguetony@gmail.com>
2022-02-13 19:46:59 +0100

    add ListingDetailsScreen & ListItem component

---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {View, StyleSheet, Image} from 'react-native'
+
+import AppText from "../components/AppText"
+import ListItem from '../components/ListItem'
+import colors from '../config/colors'
+
+function ListingDetailsScreen() {
+  return (
+    <View>
+      <Image style={styles.image} source={require('../assets/jacket.jpg')} />
+
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Red jacket for sale</AppText>
+        <AppText style={styles.price}>$100</AppText>
+
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require('../assets/avatar1.jpg')}
+            title={"John Doe"}
+            subTitle="5 Listings"
+          />
+        </View>
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20
+  },
+  image: {
+    width: '100%',
+    height: 300
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginVertical: 10
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600'
+  },
+  userContainer: {
+    marginVertical: 50
+  }
+})
+
+export default ListingDetailsScreen;
