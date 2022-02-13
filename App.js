c447da21c172cbdbf9071f4d3e6a94f2b34b877b
tony dugué <duguetony@gmail.com>
2022-02-14 00:30:27 +0100

    MessagesScreen => add flatList + Screen parent component (for padding status bar)

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />
+  return <MessagesScreen />
 }
 
---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {FlatList, StyleSheet, View} from 'react-native'
+
+import Screen from '../components/Screen'
+import ListItem from "../components/ListItem";
+import ListItemSeparator from "../components/ListItemSeparator";
+
+const messages = [
+  { id: 1, title: 'T1', description: 'D1', image: require('../assets/avatar2.jpg')},
+  { id: 2, title: 'T2', description: 'D2', image: require('../assets/avatar3.jpg')}
+]
+
+function MessagesScreen() {
+  return (
+    <Screen>
+      <FlatList
+        data={ messages }
+        keyExtractor={ message => message.id.toString() }
+        renderItem={ ({item}) => (
+          <ListItem title={item.title} subTitle={item.description} image={item.image} />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default MessagesScreen;
+
