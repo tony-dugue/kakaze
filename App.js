f5fbd6f2543c02987372d289d1a4da9dcd0f7ad3
tony dugué <duguetony@gmail.com>
2022-02-13 13:59:32 +0100

    add Card component

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import React from 'react';
+import { View } from 'react-native'
+
+import Card from './app/components/Card'
 
 export default function App() {
   return (
-      <WelcomeScreen />
+      <View style={{
+        backgroundColor: '#f8f4f4',
+        padding: 20,
+        paddingTop: 100
+      }}>
+        <Card
+          title={"Red jacket for sale"}
+          subTitle="$100"
+          image={require('./app/assets/jacket.jpg')}
+        />
+      </View>
   )
 }
 
