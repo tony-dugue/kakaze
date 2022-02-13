b47b9b10bf946b66a8f708b05c9a60a2d2cd9e88
tony dugué <duguetony@gmail.com>
2022-02-13 02:24:35 +0100

    Refactoring Colors (add global colors)

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React from 'react'
 import { View, StyleSheet, Image } from 'react-native'
+
+import colors from "../config/colors"
 
 function ViewImageScreen() {
   return (
@@ -19,19 +21,19 @@ const styles = StyleSheet.create({
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: '#fc5c65',
+    backgroundColor: colors.primary,
     position: 'absolute',
     top: 40,
     left: 30
   },
   container: {
-    backgroundColor:"#000",
+    backgroundColor: colors.black,
     flex: 1
   },
   deleteIcon: {
     width: 50,
     height: 50,
-    backgroundColor: '#4ecdc4',
+    backgroundColor: colors.secondary,
     position: 'absolute',
     top: 40,
     right: 30
