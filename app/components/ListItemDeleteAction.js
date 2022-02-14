2538197cea63f9512f468142a22fdd7e6c383262
tony dugué <duguetony@gmail.com>
2022-02-14 19:45:05 +0100

    MessagesScreen => delete items

--- app/components/ListItemDeleteAction.js
+++ app/components/ListItemDeleteAction.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, TouchableWithoutFeedback} from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 
 import colors from '../config/colors'
 
-function MyComponent() {
+function MyComponent({ onPress }) {
   return (
-    <View style={styles.container}>
-      <MaterialCommunityIcons name="trash-can" size={35} color={colors.white} />
-    </View>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons name="trash-can" size={35} color={colors.white} />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FlatList, StyleSheet, View} from 'react-native'
 
 import Screen from '../components/Screen'
 import ListItem from "../components/ListItem";
 import ListItemSeparator from "../components/ListItemSeparator";
-import listItemDeleteAction from "../components/ListItemDeleteAction";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
 
-const messages = [
+const initialMessages = [
   { id: 1, title: 'T1', description: 'D1', image: require('../assets/avatar2.jpg')},
   { id: 2, title: 'T2', description: 'D2', image: require('../assets/avatar3.jpg')}
 ]
 
 function MessagesScreen() {
+
+  const [messages, setMessages] = useState(initialMessages)
+
+  const handleDelete = message => {
+    // supprimer le message du tableau 'messages'
+    setMessages(messages.filter(m => m.id !== message.id))
+
+    // Appeler le serveur pour supprimer le message dans le backend
+  }
+
   return (
     <Screen>
       <FlatList
@@ -23,7 +33,7 @@ function MessagesScreen() {
             subTitle={item.description}
             image={item.image}
             onPress={ () => console.log('Message selected', item)}
-            renderRightActions={listItemDeleteAction}
+            renderRightActions={ () => <ListItemDeleteAction onPress={ () => handleDelete(item)} />}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
