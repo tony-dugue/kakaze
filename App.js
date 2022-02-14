fbd1812bc92e1f744a5e35a7cae92cb60df8205d
tony dugué <duguetony@gmail.com>
2022-02-14 23:39:07 +0100

    add custom component AppTextInput

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen />
+  return (
+    <Screen>
+      <AppTextInput placeholder="username" icon="email" />
+    </Screen>
+  )
 }
 
