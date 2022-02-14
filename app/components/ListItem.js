6b68b05ed48163dbfb66f0fe9974479e79366eb8
tony dugué <duguetony@gmail.com>
2022-02-14 21:01:02 +0100

    ListItem component => add condition & icon

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -5,7 +5,7 @@ import {Swipeable} from "react-native-gesture-handler";
 import AppText from './AppText'
 import colors from '../config/colors'
 
-function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
+function ListItem({ title, subTitle, image, ImageComponent, onPress, renderRightActions }) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight
@@ -14,11 +14,12 @@ function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
       >
         <View style={styles.container}>
 
-          <Image style={styles.image} source={image} />
+          {ImageComponent}
+          {image && <Image style={styles.image} source={image} />}
 
-          <View>
+          <View style={styles.detailContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
 
         </View>
@@ -31,6 +32,10 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     padding: 15
+  },
+  detailContainer: {
+    marginLeft: 10,
+    justifyContent: 'center'
   },
   image: {
     width: 70,
