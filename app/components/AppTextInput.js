d50926fac1602fa96f580ae4b0b0d7565c81211d
tony dugué <duguetony@gmail.com>
2022-02-15 20:51:18 +0100

    ListingItemScreen => add custom picker field

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -7,13 +7,19 @@ import defaultStyles from '../config/styles'
 function AppTextInput({ icon, ...otherProps }) {
   return (
     <View style={styles.container}>
+
       {icon && <MaterialCommunityIcons
         name={icon}
         size={20}
         color={defaultStyles.colors.medium}
         style={styles.icon}
       />}
-      <TextInput style={defaultStyles.text} {...otherProps} />
+
+      <TextInput
+        placeholderTextColor={defaultStyles.colors.medium}
+        style={defaultStyles.text} {...otherProps}
+      />
+
     </View>
   );
 }
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -4,13 +4,20 @@ import * as Yup from 'yup'
 
 import Screen from '../components/Screen'
 
-import { AppForm, AppFormField as FormField, SubmitButton } from '../components/forms'
+import { AppForm, AppFormField as FormField, AppFormPicker as Picker, SubmitButton } from '../components/forms'
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required("Le titre est obligatoire").min(1, "trop court").label('Titre'),
   price: Yup.number().required("Le prix est obligatoire").min(1, "trop court").max(10000, "trop long").label('Prix'),
-  description: Yup.string().label('Description')
+  description: Yup.string().label('Description'),
+  category: Yup.object().required("Choisir une catégorie").nullable().label('Catégorie')
 })
+
+const categories = [
+  { label: "Meubles", value: 1 },
+  { label: "Vêtements", value: 2 },
+  { label: "Appareils photo", value: 3 }
+]
 
 function ListingEditScreen() {
   return (
@@ -24,6 +31,8 @@ function ListingEditScreen() {
         <FormField name="title" placeholder="Titre" maxLength={255} />
 
         <FormField name="price" placeholder="Prix" keyboardType="numeric" maxLength={8} />
+
+        <Picker name="category" placeholder="Catégorie" items={categories} />
 
         <FormField name="description" placeholder="Description" maxLength={255} multiline numberOfLines={3} />
 
