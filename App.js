566dd76704b71363fbf3c8d3d09c92dc549bab8d
tony dugué <duguetony@gmail.com>
2022-02-15 01:18:51 +0100

    add custom picker + modal

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Screen from "./app/components/Screen";
+import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 
+const categories = [
+  { label: "Meubles", value: 1 },
+  { label: "Vêtements", value: 2 },
+  { label: "Appareils photo", value: 3 },
+]
+
 export default function App() {
+
+  const [category, setCategory] = useState(categories[0])
+
   return (
     <Screen>
-      <AppTextInput placeholder="username" icon="email" />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={item => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Catégorie"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   )
 }
