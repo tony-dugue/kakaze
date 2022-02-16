6e1047ecb4c54020892629b85e6b91a3c4cfa306
tony dugué <duguetony@gmail.com>
2022-02-16 02:15:20 +0100

    test add image from library (ask permissions & select image)

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
+import * as Permissions from 'expo-permissions'
+import * as ImagePicker from "expo-image-picker";
+import {Button, Image} from "react-native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from './app/components/Screen'
 
 export default function App() {
+
+  const [imageUri, setImageUri] = useState()
+
+  // demande de permission à l'utilisateur pour accéder à l'album photo et au GPS
+  const requestPermission = async () => {
+
+    const { granted } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY, Permissions.LOCATION_FOREGROUND)
+
+    if (!granted) alert('Kakaze est une application qui utilise l\'album photo ! 😜 ' +
+      'Pour continuer, nous avons besoin d\'avoir un accès à l\'album photo' )
+  }
+
+  useEffect( () => {
+    requestPermission()
+  }, [])
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync()
+      if (!result.cancelled) setImageUri(result.uri)
+    } catch (error) {
+      console.log('Erreur lors de la lecture de l\'image', error)
+    }
+  }
+
   return (
-    <ListingEditScreen />
+    <Screen>
+      <Button title={"Sélectionner une image"} onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
   )
 }
 
