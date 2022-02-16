5a69427109e8213c14662c82ac20a7015b39ca0a
tony dugué <duguetony@gmail.com>
2022-02-16 19:08:19 +0100

    add ImageInputList component

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import React, {useState} from 'react'
 
 import Screen from './app/components/Screen'
-import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
 
-  const [imageUri, setImageUri] = useState()
+  const [imageUris, setImageUris] = useState([])
+
+  const handleAdd = uri => {
+    setImageUris([...imageUris, uri])
+  }
+
+  const handleRemove = uri => {
+    setImageUris(imageUris.filter(imageUri => imageUri !== uri))
+  }
 
   return (
     <Screen>
-      <ImageInput imageUri={imageUri} onChangeImage={uri => setImageUri(uri)} />
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
     </Screen>
   )
 }
