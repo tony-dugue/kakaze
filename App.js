8e72308e3736ef1846d0f0191ab6ca325778dcfb
tony dugué <duguetony@gmail.com>
2022-02-17 02:09:37 +0100

    add Tab Navigation for listings screens

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 
-import AuthNavigator from './app/navigation/AuthNavigator'
+import AppNavigator from './app/navigation/AppNavigator'
 import navigationTheme from "./app/navigation/navigationTheme";
 
 export default function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AuthNavigator />
+      <AppNavigator />
     </NavigationContainer>
   )
 }
